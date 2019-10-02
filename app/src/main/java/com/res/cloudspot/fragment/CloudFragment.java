61a26409bdd5ec4ee5759f4e4d8f819f673f9415
ajacker <ajacker@foxmail.com>
2019-10-02 21:12:18 +0800

    介绍页面改为网页形式

--- app/src/main/java/com/res/cloudspot/fragment/CloudFragment.java
+++ app/src/main/java/com/res/cloudspot/fragment/CloudFragment.java
@@ -1,25 +1,13 @@
 package com.res.cloudspot.fragment;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ScrollView;
-import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
-import com.qmuiteam.qmui.util.QMUIDrawableHelper;
-import com.qmuiteam.qmui.util.QMUIViewHelper;
 import com.qmuiteam.qmui.widget.QMUITopBar;
-import com.qmuiteam.qmui.widget.dialog.QMUIBottomSheet;
-import com.qmuiteam.qmui.widget.dialog.QMUIDialog;
-import com.qmuiteam.qmui.widget.roundwidget.QMUIRoundButton;
+import com.qmuiteam.qmui.widget.webview.QMUIWebView;
 import com.res.cloudspot.R;
 import com.res.cloudspot.base.BaseFragment;
-import com.res.cloudspot.fragment.home.HistoryTabFragment;
-import com.res.cloudspot.util.BitmapUtil;
 import com.res.cloudspot.util.CloudData;
 
 import butterknife.BindView;
@@ -33,14 +21,8 @@ public class CloudFragment extends BaseFragment {
 
     @BindView(R.id.topbar)
     QMUITopBar mTopBar;
-    @BindView(R.id.cloudImageView)
-    ImageView mCloudImageView;
-    @BindView(R.id.titleTextView)
-    TextView mTitleTextView;
-    @BindView(R.id.commentTextView)
-    TextView mCommentTextView;
-    @BindView(R.id.cloudRootView)
-    ScrollView cloudRootView;
+    @BindView(R.id.webView)
+    QMUIWebView webView;
 
     private CloudData cloudData;
 
@@ -59,71 +41,9 @@ public class CloudFragment extends BaseFragment {
 
         mTopBar.setTitle("云朵介绍");
 
-//        mTopBar.addRightImageButton(R.mipmap.icon_topbar_overflow, R.id.topbar_right_save_button)
-//                .setOnClickListener(v -> {
-//                    showBottomSheet();
-//                });
-        Button btnShare = mTopBar.addRightTextButton("分享", QMUIViewHelper.generateViewId());
-        btnShare.setTextColor(getResources().getColor(R.color.qmui_config_color_white));
-        btnShare.setOnClickListener(v -> shareAsPic());
-    }
-
-    /**
-     * 显示底部选项
-     */
-    private void showBottomSheet() {
-        new QMUIBottomSheet.BottomListSheetBuilder(getContext())
-                .addItem("保存云朵")
-                .addItem("分享")
-                .setOnSheetItemClickListener((dialog, itemView, position, tag) -> {
-                    switch (position) {
-                        case 0:
-                            saveCloud();
-                            break;
-                        case 1:
-                            shareAsPic();
-                            break;
-                        default:
-                            break;
-                    }
-                    dialog.dismiss();
-                })
-                .build().show();
-    }
-
-    /**
-     * 生成图片分享
-     */
-    private void shareAsPic() {
-        QMUIDialog.CustomDialogBuilder dialogBuilder = new QMUIDialog.CustomDialogBuilder(getContext());
-        dialogBuilder.setLayout(R.layout.sharepic_layout);
-        final QMUIDialog dialog = dialogBuilder.setTitle("分享以下图片").create();
-
-        ImageView displayImageView = dialog.findViewById(R.id.createFromViewDisplay);
-        QMUIRoundButton shareButton = dialog.findViewById(R.id.shareButton);
-        QMUIRoundButton cancelButton = dialog.findViewById(R.id.cancelButton);
-
-        Bitmap createFromViewBitmap = QMUIDrawableHelper.createBitmapFromView(cloudRootView);
-        displayImageView.setImageBitmap(createFromViewBitmap);
-
-        displayImageView.setOnClickListener(v -> dialog.dismiss());
-        cancelButton.setOnClickListener(v -> dialog.dismiss());
-
-        shareButton.setOnClickListener(v -> BitmapUtil.share(requireContext(), createFromViewBitmap));
-
-        dialog.show();
     }
 
 
-
-    /**
-     * 保存云朵图片和介绍
-     */
-    private void saveCloud() {
-        CloudData data = new CloudData(cloudData.getBitmap(), cloudData.type);
-        data.comment = cloudData.comment;
-        HistoryTabFragment.dataList.add(data);
-    }
 
     private void initContentView() {
         Bundle data = getArguments();
@@ -131,8 +51,6 @@ public class CloudFragment extends BaseFragment {
         cloudData = (CloudData) data.getSerializable("data");
         assert cloudData != null;
 
-        Glide.with(mCloudImageView).load(cloudData.getBitmap()).into(mCloudImageView);
-        mTitleTextView.setText(cloudData.type);
-        mCommentTextView.setText(cloudData.comment);
+        webView.loadUrl("http://183.191.120.7:5500");
     }
 }
