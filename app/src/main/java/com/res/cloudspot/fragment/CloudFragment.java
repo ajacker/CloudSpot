7e9519aaaba9425ba18a1bf49f9ef93f45c6a7a4
ajacker <ajacker@foxmail.com>
2019-10-04 15:16:41 +0800

    添加了网页进度条和载入失败时候的提示 调整载入动画的文字高度

--- app/src/main/java/com/res/cloudspot/fragment/CloudFragment.java
+++ app/src/main/java/com/res/cloudspot/fragment/CloudFragment.java
@@ -1,11 +1,18 @@
 package com.res.cloudspot.fragment;
 
+import android.annotation.TargetApi;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.qmuiteam.qmui.widget.QMUITopBar;
-import com.qmuiteam.qmui.widget.webview.QMUIWebView;
 import com.res.cloudspot.R;
 import com.res.cloudspot.base.BaseFragment;
 import com.res.cloudspot.util.CloudData;
@@ -22,7 +29,9 @@ public class CloudFragment extends BaseFragment {
     @BindView(R.id.topbar)
     QMUITopBar mTopBar;
     @BindView(R.id.webView)
-    QMUIWebView webView;
+    WebView webView;
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
 
     private CloudData cloudData;
 
@@ -44,13 +53,63 @@ public class CloudFragment extends BaseFragment {
     }
 
 
-
     private void initContentView() {
         Bundle data = getArguments();
         assert data != null;
         cloudData = (CloudData) data.getSerializable("data");
         assert cloudData != null;
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                if (newProgress == 100) {
+                    progressBar.setVisibility(View.GONE);
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setProgress(newProgress);
+                }
+            }
+        });
+
+        webView.setWebViewClient(new WebViewClient() {
+
+
+            @TargetApi(21)
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                super.onReceivedError(view, errorCode, description, failingUrl);
+                if (errorCode == ERROR_HOST_LOOKUP || errorCode == ERROR_CONNECT || errorCode == ERROR_TIMEOUT) {
+                    view.loadUrl("about:blank");
+                    view.loadDataWithBaseURL(null, "页面加载出错，请检查网络连接", "text/html", "utf-8", null);
+                    //view.loadUrl(mErrorUrl);
+                }
+            }
+
+            @TargetApi(23)
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                int errorCode = error.getErrorCode();
+                if (errorCode == ERROR_HOST_LOOKUP || errorCode == ERROR_CONNECT || errorCode == ERROR_TIMEOUT) {
+                    view.loadUrl("about:blank");
+                    view.loadDataWithBaseURL(null, "页面加载出错，请检查网络连接", "text/html", "utf-8", null);
+                    //view.loadUrl(mErrorUrl);
+                }
+            }
+
+            @Override
+            public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
+                super.onReceivedHttpError(view, request, errorResponse);
+                int statusCode = errorResponse.getStatusCode();
+                if (404 == statusCode || 500 == statusCode) {
+                    view.loadUrl("about:blank");// 避免出现默认的错误界面
+                    view.loadDataWithBaseURL(null, "错误代码:" + statusCode, "text/html", "utf-8", null);
+                    //view.loadUrl(mErrorUrl);
+                }
+            }
+        });
 
         webView.loadUrl("http://ajacker.tpddns.cn:5500/" + cloudData.type + ".html");
+
     }
 }
